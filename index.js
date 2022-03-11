aa846332676d79fdc0f3ce4d245b2ad306996143
Dylan Temples <dtemps123@gmail.com>
2022-03-11 18:22:28 +0000

    updates, idk

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const addon = require( './build/Release/greet.node' );
+const addon = require('bindings')('greet'); // import 'greet.node'
 exports.hello = addon.greetHello;
 
 // see properties on 'exports' object
-console.log( 'exports : ', addon );
-console.log();
+// console.log( 'exports : ', addon );
+// console.log();
 
 // exectute the 'greetHello' function
 // console.log( 'greetModule.greetHello() : ', greetModule.greetHello( "Big Dyl" ) );
