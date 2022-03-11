97556aa33d24f055d06a50810313371118212238
Dylan Temples <dtemps123@gmail.com>
2022-03-11 17:58:14 +0000

    create a js wrapper script to call our native addon

---
+++ index.js
@@ -0,0 +1,9 @@
+const greetModule = require( './build/Release/greet.node' );
+
+// see properties on 'exports' object
+console.log( 'exports : ', greetModule );
+console.log();
+
+// exectute the 'greetHello' function
+console.log( 'greetModule.greetHello() : ', greetModule.greetHello() );
+console.log();
